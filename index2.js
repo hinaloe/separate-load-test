b0f35eb84c2a3e6e60e126188be83ec7c63e12ab
hina <hina@hinaloe.net>
2016-10-11 19:18:38 +0900

    モジュールキャッシュを理解できる

--- index2.js
+++ index2.js
@@ -4,3 +4,20 @@ const {noop} = require('./load-getter');
 console.log('loaded modules');
 
 noop('test');
+
+console.log('重い方も試す');
+
+const load = require('./load-getter');
+
+console.log('1回目');
+load.heavy('test1');
+console.log('2回目');
+load.heavy('test2');
+
+const load2 = require('./load-getter');
+
+console.log('3回目');
+load2.heavy('test3');
+console.log('4回目');
+load2.heavy('test4');
+console.log('end');
